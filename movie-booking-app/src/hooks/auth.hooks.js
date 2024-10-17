53ad2fddf1d9c89688194c830c32b31fa788d1f4
vikijai <vigneshjai98@gmail.com>
2024-10-17 21:58:48 +0530

    signin added

--- movie-booking-app/src/hooks/auth.hooks.js
+++ movie-booking-app/src/hooks/auth.hooks.js
@@ -23,6 +23,25 @@ export const useSignup = () => {
   return mutation;
 };
 
+export const useSignin = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async ({ email, password }) => {
+      const { data } = await apiInstance.post("/auth/sign-in", {
+        email,
+        password,
+      });
+      const token = data.data.token;
+      if (token) localStorage.setItem("token", token);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+  });
+  return mutation;
+};
+
 export const useLoggedInUser = () => {
   const query = useQuery({
     queryKey: ["user"],
--- movie-booking-app/src/pages/sign-in/SignIn.jsx
+++ movie-booking-app/src/pages/sign-in/SignIn.jsx
@@ -1,11 +1,68 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+import { useState } from "react";
+
+import { useLoggedInUser, useSignin } from "../../hooks/auth.hooks";
+import { useEffect } from "react";
+
 
 const SigninPage = () => {
+  const navigate = useNavigate();
+
+  const { mutateAsync: signinAsync } = useSignin();
+  const { data: loggedInUser } = useLoggedInUser();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (loggedInUser) navigate("/dashboard");
+  }, [loggedInUser, navigate]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    await signinAsync({ email, password });
+  };
+
+
   return (
-    <div>
-      Sign in Page
-      <Button variant="contained">Cool</Button>
+    <div className="signup-page-container">
+      <div>
+        <Typography variant="h3">Sign In</Typography>
+        <Box component="form" onSubmit={handleFormSubmit}>
+          <div className="form-row">
+            <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              label="Email Address"
+              type="email"
+              required
+            />
+          </div>
+          <div className="form-row">
+            <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              label="Password"
+              type="password"
+              required
+            />
+          </div>
+
+          <div className="form-row">
+            <Button type="submit" fullWidth variant="contained">
+              Sign In
+            </Button>
+          </div>
+        </Box>
+      </div>
     </div>
+
   );
 };
 
